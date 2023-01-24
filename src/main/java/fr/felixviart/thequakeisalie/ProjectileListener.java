bef1b001fb464121c5830f5aa5c2a49b80c4c173
ViartFelix <viart.felix2017@gmail.com>
2023-01-24 15:56:06 +0100

    MàJ: Flèche qui traverse les entités et début de l'option fantôme

--- src/main/java/fr/felixviart/thequakeisalie/ProjectileListener.java
+++ src/main/java/fr/felixviart/thequakeisalie/ProjectileListener.java
@@ -1,9 +1,7 @@
 package fr.felixviart.thequakeisalie;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Sound;
+import org.bukkit.*;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -15,53 +13,76 @@ import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.util.Vector;
 
 import java.lang.reflect.Array;
+import java.util.Optional;
 
 public class ProjectileListener implements Listener {
     @EventHandler
-    public void ArrowShootEvent(ProjectileHitEvent event){
+    public void ArrowShootEvent(ProjectileHitEvent event) {
         Entity entity = event.getEntity();
         //Si l'entité qui damage est une flèche
         if (event.getEntity() instanceof Arrow){
             Arrow arrow = (Arrow) entity;
 
             //Si l'entité qui a tirée la flèche est un joueur et qu'elle touche un autre entité
-            if(arrow.getShooter() instanceof Player && event.getHitEntity() != null){
-                Entity shooter = (Entity) arrow.getShooter();
-                Player player = (Player) shooter;
-                Location player_coords=player.getLocation();
+            if(arrow.getShooter() instanceof Player){
+                if(event.getHitEntity() != null) {
+                    Entity shooter = (Entity) arrow.getShooter();
+                    Player player = (Player) shooter;
+                    Location player_coords=player.getLocation();
 
-                //Si le joueur a une houe en fer dans la main
-                if(player.getInventory().getItemInMainHand().getType()==Material.IRON_HOE) {
-                    player.playSound(player.getLocation(),Sound.ENTITY_ARROW_HIT_PLAYER,0.5f,1f);
-                    //Bukkit.broadcastMessage("Entité touché par le joueur !");
+                    //Si le joueur a une houe en fer dans la main
+                    if(player.getInventory().getItemInMainHand().getType()==Material.IRON_HOE) {
+                        player.playSound(player.getLocation(),Sound.ENTITY_ARROW_HIT_PLAYER,0.5f,1f);
 
-                    /* Solution 1
-                    Location arrow_loc=arrow.getLocation();
-                    Vector arrow_loc_vec=arrow.getLocation().getDirection();
-                    arrow.remove();
-                    Projectile projectile=player.launchProjectile(Arrow.class,arrow_loc_vec);
-                    projectile.setBounce(false);
-                    projectile.teleport(arrow_loc);
-                    projectile.setVelocity(arrow_loc_vec);
-                     */
+                        World world=player.getWorld();
+                        Location arrow_loc=arrow.getLocation();
+                        arrow.remove();
 
-                    /* Solution 2
-                    Location arrow_loc=arrow.getLocation();
-                    arrow.remove();
-                    double n_arrow_X=Math.cos(Math.toRadians(arrow_loc.getYaw()));
-                    double n_arrow_Z=Math.sin(Math.toRadians(arrow_loc.getYaw()));
+                        Projectile projectile=player.launchProjectile(Arrow.class);
+                        projectile.setVelocity(arrow.getVelocity().normalize().multiply(2));
+                        projectile.teleport(arrow_loc.add(arrow.getVelocity().normalize().multiply(3)));
 
-                    Vector arrowDir=new Vector(n_arrow_X,arrow_loc.getY(),n_arrow_Z);
-                    Projectile projectile=player.launchProjectile(Arrow.class);
-                    projectile.setVelocity(arrowDir);
-                    projectile.teleport(arrow_loc);
-                     */
+                        /*
+                        world.spawnArrow(arrow.getLocation().add(arrow.getVelocity().normalize().multiply(2)),arrow.getVelocity(),2f,12f);
+                        arrow.setShooter(player);
+                        */
+
+                    }
+                } else if (event.getHitEntity() == null) {
+                    Bukkit.broadcastMessage("Tentative traverser block");
+                        /*
+                        Location arrow_loc=arrow.getLocation();
+
+                        Double final_x=arrow_loc.getX();
+                        Double final_y;
+                        Double final_z=arrow_loc.getZ();
+
+                        BlockFace arrow_facing=arrow.getFacing();
+
+                        Location block_hit=arrow.getLocation().getBlock().getLocation();
+
+                        switch (arrow_facing) {
+                            case EAST:
+                                final_x=arrow_loc.getX()+shooted.getBoundingBox().getWidthX()+0.25f;
+                                break;
+                            case WEST:
+                                final_x=arrow_loc.getX()-shooted.getBoundingBox().getWidthX()-0.25f;
+                                break;
+                            case NORTH:
+                                final_z=arrow_loc.getZ()-shooted.getBoundingBox().getWidthZ()-0.25f;
+                                break;
+                            case SOUTH:
+                                final_z=arrow_loc.getZ()+shooted.getBoundingBox().getWidthZ()+0.25f;
+                                break;
+                        }
+
+                         */
                 }
             }
         }
     }
 
-    /*
+    /* Pour après
     @EventHandler
     public void EntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
 
